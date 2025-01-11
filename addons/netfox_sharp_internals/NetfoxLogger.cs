62ba013ae9ea1718f7f283fb9c5aaa513fe9c611
Dustie <77035922+DustieDog@users.noreply.github.com>
2025-01-11 02:55:15 -0330

    Updated Phrasing
    Updated phrasing for summaries.

--- addons/netfox_sharp_internals/NetfoxLogger.cs
+++ addons/netfox_sharp_internals/NetfoxLogger.cs
@@ -8,8 +8,6 @@ public class NetfoxLogger
     static GodotObject _staticLogger;
     GodotObject _logger;
 
-    const int DefaultLogLevel = 2;
-
     /// <summary>The minimum level needed to print a given log. Logs must also meet their
     /// respective <see cref="ModuleLogLevel"/>, if applicable.</summary>
     public static int LogLevel
@@ -24,13 +22,13 @@ public class NetfoxLogger
         get { return (Dictionary)_staticLogger?.Get(PropertyNameGd.ModuleLogLevel); ; }
         set { _staticLogger?.Set(PropertyNameGd.ModuleLogLevel, value); }
     }
-    /// <summary>The addon/module that the logger comes from.</summary>
+    /// <summary>The addon/module that the logger belongs to.</summary>
     public string Module
     {
         get { return (string)_logger.Get(PropertyNameGd.Module); ; }
         set { _logger.Set(PropertyNameGd.Module, value); }
     }
-    /// <summary>The name of the node that the logger comes from.</summary>
+    /// <summary>The name of the node.</summary>
     public string Name
     {
         get { return (string)_logger.Get(PropertyNameGd.Name); ; }
@@ -44,23 +42,23 @@ public class NetfoxLogger
     }
 
     /// <summary>Internal constructor for instance instantiation.</summary>
-    NetfoxLogger(string module, string name)
+    public NetfoxLogger(string module, string name)
     {
         _logger = (GodotObject)GD.Load<GDScript>("res://addons/netfox.internals/logger.gd").New(module, name);
     }
 
-    /// <summary>Creates a new logger that uses netfox as the module.</summary>
-    /// <param name="nodeName">The name of the node this logger is used for.</param>
+    /// <summary>Creates a new logger for the netfox module.</summary>
+    /// <param name="name">The name of the logger.</param>
     /// <returns>The logger instance.</returns>
-    public static NetfoxLogger ForNetfox(string nodeName) { return new("netfox", nodeName); }
-    /// <summary>Creates a new logger that uses netfox.noray as the module.</summary>
-    /// <param name="nodeName">The name of the node this logger is used for.</param>
+    public static NetfoxLogger ForNetfox(string name) { return new("netfox", name); }
+    /// <summary>Creates a new logger for the netfox.noray module.</summary>
+    /// <param name="name">The name of the logger.</param>
     /// <returns>The logger instance.</returns>
-    public static NetfoxLogger ForNoray(string nodeName) { return new("netfox.noray", nodeName); }
-    /// <summary>Creates a new logger that uses netfox.extras as the module.</summary>
-    /// <param name="nodeName">The name of the node this logger is used for.</param>
+    public static NetfoxLogger ForNoray(string name) { return new("netfox.noray", name); }
+    /// <summary>Creates a new logger for the netfox.extras module.</summary>
+    /// <param name="name">The name of the logger.</param>
     /// <returns>The logger instance.</returns>
-    public static NetfoxLogger ForExtras(string nodeName) { return new("netfox.extras", nodeName); }
+    public static NetfoxLogger ForExtras(string name) { return new("netfox.extras", name); }
     public static Dictionary MakeSetting(string name) { return (Dictionary)_staticLogger.Call(MethodNameGd.MakeSetting, name); }
     public static void RegisterTag(Callable tag, int priority = 0) { _staticLogger.Call(MethodNameGd.RegisterTag, tag, priority); }
 
